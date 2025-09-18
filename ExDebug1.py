3578db5fb10872c4f469a790420d6678639cf81b
Adem777777 <2580776@cegepoutaouais.qc.ca>
2025-09-18 11:44:07 -0400

    - Ajout des inputs - ordi liste ajouté

--- ExDebug1.py
+++ ExDebug1.py
@@ -15,10 +15,10 @@ def environnement_optimal(temp, poussiere, humidite):
     alerte = False
 
     # Vérification température
-    if temp < 18:
+    if temp <= 18:
         print("Température trop basse")
         alerte = True
-    elif temp > 27:
+    elif temp >= 27:
         print("Température trop élevée")
         alerte = True
 
@@ -41,10 +41,25 @@ def environnement_optimal(temp, poussiere, humidite):
     else:
         return "Environnement non optimal"
 
+while True:
+    if __name__ == "__main__":
 
-if __name__ == "__main__":
-   #  print(environnement_optimal(25, "faible", 40))
-    temp = int(input("Entrez la température désirée:"))
-    poussiere = input("Entrez le niveau de poussiere:")
-    humidite = float(input("Entrez le niveau d'humidité:"))
-    print(environnement_optimal(temp, poussiere, humidite))
+    #  print(environnement_optimal(25, "faible", 40))
+        #temp = int(input("Entrez la température désirée:"))
+        #poussiere = input("Entrez le niveau de poussiere:")
+        #humidite = float(input("Entrez le niveau d'humidité:"))
+        ordi1 = input("Entrez la température, niveau de poussière et niveau d'humidité séparé avec des espaces.")
+        liste_ordi1 = ordi1.split(" ")
+        ordi2 = input("Entrez la température, niveau de poussière et niveau d'humidité séparé avec des espaces pour le deuxième ordi.")
+        liste_ordi2 = ordi2.split(" ")
+        ordi3 = input("Entrez la température, niveau de poussière et niveau d'humidité séparé avec des espaces pour le troisième ordi.")
+        liste_ordi3 = ordi3.split(" ")
+
+
+
+        #print(environnement_optimal(temp, poussiere, humidite))
+        print(environnement_optimal(float(liste_ordi1[0]), liste_ordi1[1], float(liste_ordi1[2])))
+
+        print(environnement_optimal(float(liste_ordi2[0]), liste_ordi2[1], float(liste_ordi2[2])))
+
+        print(environnement_optimal(float(liste_ordi3[0]), liste_ordi3[1], float(liste_ordi3[2])))
